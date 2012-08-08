6ee9968ec52241e8fae7386059ffbf13b98a5200
James Buren <ryuo@frugalware.org>
2012-08-08 13:06:58 -0500

    start over

--- backend.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-extern int backend_log_to_file(const char *path);
-extern int backend_set_log_to_memory(void);
-extern int backend_log(const char *format,...) __attribute__((format(printf,1,2)));
