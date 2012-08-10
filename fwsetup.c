d3757475bd66057a0205a0ef4b4d8f761561272f
James Buren <ryuo@frugalware.org>
2012-08-10 16:55:16 -0500

    wee

---
+++ fwsetup.c
@@ -0,0 +1,13 @@
+#include "fwsetup.h"
+
+extern int main(void)
+{
+  struct database db = {0};
+
+#ifdef NEWT
+#endif
+
+  return EXIT_SUCCESS;
+}
+
+// -%- strip: yes; add-newline: yes; use-tabs: no; indent-width: 2; tab-width: 2; -%-
---
+++ fwsetup.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <assert.h>
+
+#ifdef NEWT
+#include <newt.h>
+
+struct database
+{
+  int screen_width;
+  int screen_height;
+  int window_width;
+  int window_height;
+  int window_x;
+  int window_y;
+};
+#else
+#error "No known user interface is defined."
+#endif
+
+struct module
+{
+  bool (*run) (struct database *);
+};
+
+extern int main(void);
+
+// -%- strip: yes; add-newline: yes; use-tabs: no; indent-width: 2; tab-width: 2; -%-
