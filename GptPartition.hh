62020326bdb7498063097a34527dad4e602d62bc
James Buren <ryuo@frugalware.org>
2012-08-18 21:54:56 -0500

    new partition type for GPT

---
+++ GptPartition.hh
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Partition.hh"
+
+class GptPartition : public Partition
+{
+
+public:
+  GptPartition() { _gpt_flags = 0; }
+  ~GptPartition() {}
+  void setType(const string &type) { _gpt_type = type; }
+  void setName(const string &name) { _gpt_name = name; }
+  void setUUID(const string &uuid) { _gpt_uuid = uuid; }
+  void setFlags(unsigned long long flags) { _gpt_flags = flags; }
+  string getType() { return _gpt_type; }
+  string getName() { return _gpt_name; }
+  string getUUID() { return _gpt_uuid; }
+  unsigned long long getFlags() { return _gpt_flags; } 
+
+protected:
+  string _gpt_type;
+  string _gpt_name;
+  string _gpt_uuid;
+  unsigned long long _gpt_flags;
+
+};
