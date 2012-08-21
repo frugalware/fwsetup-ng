03ee512a3f8eabd27b85035525dc4a4ae50403a5
James Buren <ryuo@frugalware.org>
2012-08-21 14:26:59 -0500

    fix setting of active in dospartition

--- DosPartition.hh
+++ DosPartition.hh
@@ -9,7 +9,7 @@ public:
   DosPartition() { _dos_type = 0, _dos_active = false; }
   virtual ~DosPartition() { }
   void setType(unsigned char type) { _dos_type = type; }
-  virtual void setActive(bool active) { _dos_active = true; }
+  virtual void setActive(bool active) { _dos_active = active; }
   unsigned char getType() { return _dos_type; }
   virtual bool getActive() { return _dos_active; }
 
