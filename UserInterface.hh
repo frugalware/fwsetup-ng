acf84aeaa66973fe1795459f90e61d696790ce92
James Buren <ryuo@frugalware.org>
2012-08-24 16:18:58 -0500

    add user interface functions for retrieving data

--- UserInterface.hh
+++ UserInterface.hh
@@ -7,6 +7,12 @@ public:
   UserInterface();
   ~UserInterface();
   bool initialize(int argc,char **argv);
+  int getScreenWidth() { return _s_width; }
+  int getScreenHeight() { return _s_height; }
+  int getWindowWidth() { return _w_width; }
+  int getWindowHeight() { return _w_height; }
+  int getWindowXOffset() { return _x; }
+  int getWindowYOffset() { return _y; }
 
 private:
   int _s_width;
