6beeefc6270ef0427b47ad968f10d9ccef54d6b1
James Buren <ryuo@frugalware.org>
2012-08-24 23:40:19 -0500

    implement getActive/setActive for GptPartition

--- GptPartition.hh
+++ GptPartition.hh
@@ -12,10 +12,12 @@ public:
   void setName(const string &name) { _gpt_name = name; }
   void setUUID(const string &uuid) { _gpt_uuid = uuid; }
   void setFlags(unsigned long long flags) { _gpt_flags = flags; }
+  virtual void setActive(bool active) { if(active) _gpt_flags |= 4; else if(_gpt_flags & 4) _gpt_flags ^= 4; }
   string getType() { return _gpt_type; }
   string getName() { return _gpt_name; }
   string getUUID() { return _gpt_uuid; }
   unsigned long long getFlags() { return _gpt_flags; } 
+  virtual bool getActive() { return (_gpt_flags & 4) ? true : false; }
 
 protected:
   string _gpt_type;
