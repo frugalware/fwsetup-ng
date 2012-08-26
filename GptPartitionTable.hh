d34db915fb550616f52281c1672a4188bf2fbce2
James Buren <ryuo@frugalware.org>
2012-08-25 19:43:36 -0500

    remove old API for partition data retrieval. new API shall retrieve partitions and use the partition API directly.

--- GptPartitionTable.hh
+++ GptPartitionTable.hh
@@ -11,12 +11,6 @@ public:
   virtual bool read(const string &path);
   virtual bool write(const string &path);  
   virtual Partition *newPartition();
-  virtual unsigned long long getNumber(unsigned long long n);
-  virtual unsigned long long getStart(unsigned long long n);
-  virtual unsigned long long getEnd(unsigned long long n);
-  virtual unsigned long long getSectors(unsigned long long n);
-  virtual string getType(unsigned long long n);
-  virtual bool getActive(unsigned long long n);
 
 protected:
   string _uuid;
--- PartitionTable.hh
+++ PartitionTable.hh
@@ -17,12 +17,6 @@ public:
   size_t getTableSize() { return _table.size(); }
   Partition *getPartition(size_t n) { return _table.at(n); }
   virtual Partition *newPartition() { return 0; }
-  virtual unsigned long long getNumber(unsigned long long n) { return 0; }
-  virtual unsigned long long getStart(unsigned long long n) { return 0; }
-  virtual unsigned long long getEnd(unsigned long long n) { return 0; }
-  virtual unsigned long long getSectors(unsigned long long n) { return 0; }
-  virtual string getType(unsigned long long n) { return ""; }
-  virtual bool getActive(unsigned long long n) { return false; }
 
 protected:
   string _label;
