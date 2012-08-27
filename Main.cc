87f27750b2357272f36e079fffec058580f9dce9
James Buren <ryuo@frugalware.org>
2012-08-27 17:37:47 -0500

    check if we're running as root and setup locale

--- Main.cc
+++ Main.cc
@@ -1,11 +1,19 @@
 #include <stdlib.h>
+#include <unistd.h>
+#include <locale.h>
 #include "UserInterface.hh"
 #include "Utility.hh"
 
 int main(int argc,char **argv)
 {
+  if(geteuid() != 0)
+    return EXIT_FAILURE;
+
+  if(setlocale(LC_ALL,"") == 0)
+    return EXIT_FAILURE;
+
   if(!ui.initialize(argc,argv))  
-    return false;
+    return EXIT_FAILURE;
 
   return EXIT_SUCCESS;
 }
