4e0669e3219b80dafa7a38193939f5e88a167b1f
James Buren <ryuo@frugalware.org>
2012-08-28 01:57:39 -0500

    add code to cycling through the module units

--- Main.cc
+++ Main.cc
@@ -3,9 +3,21 @@
 #include <locale.h>
 #include "UserInterface.hh"
 #include "Utility.hh"
+#include "PartitionModule.hh"
+
+static Module edge_guard_module;
+
+static Module modules[] =
+{
+  edge_guard_module,
+  partition_module,
+  edge_guard_module
+};
 
 int main(int argc,char **argv)
 {
+  Module *module = &modules[1];
+
   if(geteuid() != 0)
     return EXIT_FAILURE;
 
@@ -14,6 +26,18 @@ int main(int argc,char **argv)
 
   if(!ui.initialize(argc,argv))
     return EXIT_FAILURE;
+
+  while(module->getName() != "")
+  {
+    int i = module->run();
+    
+    if(i < 0)
+      --module;
+    else if(i > 0)
+      ++module;
+    else if(i == 0)
+      break;
+  }
 
   return EXIT_SUCCESS;
 }
