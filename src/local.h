bb998b5344dcf9497e9e8a2f98cb50080b8827ae
James Buren <ryuo@frugalware.org>
2012-08-28 23:50:53 -0500

    hm forgot header

--- src/local.h
+++ src/local.h
@@ -2,6 +2,13 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
+#define _(S) S
+#define NEWT_WIDTH  70
+#define NEWT_HEIGHT 21
 
 extern int ui_main(int argc,char **argv);
+extern int main(int argc,char **argv);
