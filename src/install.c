31d7fba0238980ccf47f03789c74bcdc4d256aa0
James Buren <ryuo@frugalware.org>
2012-08-29 23:42:19 -0500

    also record an ETA estimate

--- src/install.c
+++ src/install.c
@@ -8,6 +8,7 @@ struct dldata
   int percent;
   char percent_text[5];
   float timediff;
+  char rate_text[47];
 };
 
 static PM_DB **databases = 0;
@@ -48,9 +49,30 @@ static int install_download_callback(PM_NETBUF *ctl,int dl_xfered0,void *arg)
 
   dl.percent = (float) dl.amount / dl.total * 100;
 
+  snprintf(dl.percent_text,5,"%d%%",dl.percent);
+
   gettimeofday(&dl_time1,0);
 
   dl.timediff = (dl_time1.tv_sec - dl_time0.tv_sec) + (float) (dl_time1.tv_usec - dl_time0.tv_usec) / 1000000;
+
+  if(dl.amount == dl.total)
+  {
+    dl_rate = dl_xfered0 / (dl.timediff * KIBIBYTE);
+    dl_eta_h = (int) dl.timediff / 3600;
+    dl_eta_m = (int) dl.timediff % 3600 / 60;
+    dl_eta_s = (int) dl.timediff % 3600 % 60;
+  }
+  else if(dl.timediff > 1.0)
+  {
+    dl_rate = (dl_xfered0 - dl_xfered1) / (dl.timediff * KIBIBYTE);
+    dl_xfered1 = dl_xfered0;
+    gettimeofday(&dl_time0,0);
+    dl_eta_h = (int) ((dl.total - dl.amount) / (dl_rate * KIBIBYTE)) / 3600;
+    dl_eta_m = (int) ((dl.total - dl.amount) / (dl_rate * KIBIBYTE)) % 3600 / 60;
+    dl_eta_s = (int) ((dl.total - dl.amount) / (dl_rate * KIBIBYTE)) % 3600 % 60;
+  }
+
+  snprintf(dl.rate_text,47,"%.1fKiB/s",dl_rate);
 
   return 1;
 }
