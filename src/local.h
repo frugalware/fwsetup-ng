00c72c9e1febc0cbd73d2422196e4171298e9ef9
James Buren <ryuo@frugalware.org>
2012-08-29 22:30:03 -0500

    import byte macros

--- src/local.h
+++ src/local.h
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <errno.h>
 #include <limits.h>
 
@@ -14,6 +15,10 @@
 #define INSTALL_ROOT "/home/ryuo/fwsetup-ng/root"
 #define NEWT_WIDTH  70
 #define NEWT_HEIGHT 21
+#define KIBIBYTE (1LLU << 10LLU)
+#define MEBIBYTE (1LLU << 20LLU)
+#define GIBIBYTE (1LLU << 30LLU)
+#define TEBIBYTE (1LLU << 40LLU)
 
 extern bool mkdir_recurse(const char *path);
 extern int ui_main(int argc,char **argv);
