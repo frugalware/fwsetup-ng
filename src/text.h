8ff29c235b59628126da448d6802f4a37c7830fa
James Buren <ryuo@frugalware.org>
2012-08-31 02:19:53 -0500

    add some more text macros

--- src/text.h
+++ src/text.h
@@ -5,8 +5,17 @@
 #define OK_BUTTON_TEXT _("Ok")
 #define YES_BUTTON_TEXT _("Yes")
 #define NO_BUTTON_TEXT _("No")
+#define PREVIOUS_BUTTON_TEXT _("Previous")
+#define NEXT_BUTTON_TEXT _("Next")
 
 #define PROGRESS_DIALOG_CANCEL_TEXT _("Progress dialog callback canceled the operation.\n")
 #define NEWT_INIT_FAILURE_TEXT _("Could not initialize the NEWT user interface.\n")
 #define NEWT_TOO_SMALL_TEXT _("We require a terminal of 80x24 or greater to use the NEWT user interface.\n")
 #define NEWT_WINDOW_FAILURE_TEXT _("Failed to open a NEWT window.\n")
+
+#define INSTALL_UPDATE_TEXT _("Update package databases")
+#define INSTALL_WINDOW_TEXT _( \
+"It is now time to select which package groups to install. If you are\n" \
+"wanting to perform a network install, please make sure that the\n" \
+"checkbox for updating the databases is checked.\n" \
+)
