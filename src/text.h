2ae906e939a46417796601579b56ea4543d6d7a0
James Buren <ryuo@frugalware.org>
2012-09-05 00:58:19 -0500

    update text macros

--- src/text.h
+++ src/text.h
@@ -13,9 +13,7 @@
 #define NEWT_TOO_SMALL_TEXT _("We require a terminal of 80x24 or greater to use the NEWT user interface.\n")
 #define NEWT_WINDOW_FAILURE_TEXT _("Failed to open a NEWT window.\n")
 
-#define INSTALL_UPDATE_TEXT _("Update package databases")
+#define INSTALL_TITLE_TEXT _("Package Group Selection")
 #define INSTALL_WINDOW_TEXT _( \
-"It is now time to select which package groups to install. If you are\n" \
-"wanting to perform a network install, please make sure that the\n" \
-"checkbox for updating the databases is checked.\n" \
+"It is now time to select which package groups to install.\n" \
 )
