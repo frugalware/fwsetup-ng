ad3727566b7100e5fbf4f0bd588b7507e9df4494
James Buren <ryuo@frugalware.org>
2012-09-26 12:30:26 -0500

    initial draft of disk open using blkid

--- src/block.c
+++ src/block.c
@@ -1,4 +1,5 @@
 #include <linux/major.h>
+#include <blkid.h>
 #include "local.h"
 
 enum devicetype
@@ -29,8 +30,8 @@ struct partition
   struct disk *disk;
   char number;
   long long start;
-  long long end;
   long long size;
+  long long end;
   unsigned char dostype;
   bool dosactive;
   char gptname[37];
@@ -182,4 +183,118 @@ extern void device_close(struct device *device)
   free(device->path);
   
   free(device);
+}
+
+extern struct disk *disk_open(const struct device *device)
+{
+  int fd = -1;
+  blkid_probe probe = 0;
+  blkid_partlist partlist = 0;
+  blkid_parttable parttable = 0;
+  const char *label = 0;
+  struct disk disk = {0};
+  int i = 0;
+  int j = 0;
+  blkid_partition partition = 0;
+
+  if(device == 0)
+  {
+    errno = EINVAL;
+    fprintf(logfile,"%s: %s\n",__func__,strerror(errno));
+    goto bail;
+  }
+
+  if((fd = open(device->path,O_RDONLY)) == -1)
+  {
+    fprintf(logfile,"%s: %s\n",__func__,strerror(errno));
+    goto bail;
+  }
+
+  if((probe = blkid_new_probe()) == 0)
+  {
+    fprintf(logfile,"%s: %s\n",__func__,strerror(errno));
+    goto bail;
+  }
+
+  if(blkid_probe_set_device(probe,fd,0,0) == -1)
+  {
+    errno = EINVAL;
+    fprintf(logfile,"%s: %s\n",__func__,strerror(errno));
+    goto bail;
+  }
+
+  if(
+    (partlist = blkid_probe_get_partitions(probe))   == 0 ||
+    (parttable = blkid_partlist_get_table(partlist)) == 0 ||
+    (label = blkid_parttable_get_type(parttable))    == 0
+  )
+  {
+    fprintf(logfile,"%s: %s\n",__func__,_("no partition table"));
+    goto bail;
+  }
+
+  if(strcmp(label,"dos") == 0)
+    disk.type = DISKTYPE_DOS;
+  else if(strcmp(label,"gpt") == 0)
+    disk.type = DISKTYPE_GPT;
+  else
+  {
+    fprintf(logfile,"%s: %s\n",__func__,_("unknown partition table"));
+    goto bail;
+  }
+
+  if((j = blkid_partlist_numof_partitions(partlist)) > 0)
+  {
+    if((disk.type == DISKTYPE_DOS && j > 60) || (disk.type == DISKTYPE_GPT && j > 128))
+    {
+      fprintf(logfile,"%s: %s\n",__func__,_("partition table too big"));
+      goto bail;
+    }
+    
+    for( ; i < j && (partition = blkid_partlist_get_partition(partlist,i)) != 0 ; ++i )
+    {
+      struct partition *part = &disk.table[i];
+      
+      part->number = blkid_partition_get_partno(partition);
+      
+      part->start = blkid_partition_get_start(partition);
+      
+      part->size = blkid_partition_get_size(partition);
+      
+      part->end = part->start + part->size - 1;
+      
+      if(disk.type == DISKTYPE_DOS)
+      {
+        part->dostype = blkid_partition_get_type(partition);
+        
+        part->dosactive = (blkid_partition_get_flags(partition) == 0x80);
+        
+        continue;
+      }
+      
+      if(disk.type == DISKTYPE_GPT)
+      {
+        if(blkid_partition_get_name(partition) != 0)
+          snprintf(part->gptname,37,"%s",blkid_partition_get_name(partition));
+
+        snprintf(part->gptuuid,37,"%s",blkid_partition_get_uuid(partition));
+
+        snprintf(part->gpttype,37,"%s",blkid_partition_get_type_string(partition));
+        
+        part->gptflags = blkid_partition_get_flags(partition);
+        
+        continue;
+      }
+    }
+  }
+
+bail:
+
+  if(fd != -1)
+    close(fd);
+  
+  if(probe != 0)
+    blkid_free_probe(probe);
+  
+  return 0;
 }
